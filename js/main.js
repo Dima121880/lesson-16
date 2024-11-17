532cc638bbe68cc6083c4dbc2a4b682b2516d69d
Dima121880 <surguladze.dima33@gmail.com>
2024-11-17 23:06:12 +0400

    add html and js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const myarray = [5, 10, 14, 5, 73];
+console.log(5 + 10 + 14 + 5 + 73);
+const myarray1 = 107;
+console.log(myarray1 / 5);
+
+let booktitle = {
+  title: "world",
+  author: "richard",
+  yearPublished: 2024,
+};
+booktitle.title = "world by";
+booktitle.author = " richard was published in ";
+console.log(booktitle.title + booktitle.author + booktitle.yearPublished);
+
+booktitle.yearPublished = 2025;
+console.log(booktitle);
+
+const myarray3 = {
+  name: ["Dima"],
+  age: [16],
+  score: [2025],
+};
+console.log(myarray3);
